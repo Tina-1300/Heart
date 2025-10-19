60a73b228add222ba20ecb4217c8ab543ec5e045
Tina-1300 <tina.xytrfgthuji1348@gmail.com>
2025-10-19 11:34:39 +0200

    update(brokenheart)

--- HeartShape.hpp
+++ HeartShape.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <cmath>
-
+#include <vector>
 
 class HeartShape {
     public:
@@ -29,15 +29,12 @@ class HeartShape {
 
 };
 
-// to do : heart break
+
 class BreakHeartShape{
     public:
-        static sf::ConvexShape create(float x, float y, float scale){
+        static std::vector<sf::ConvexShape> create(float x, float y, float scale){
 
-            sf::ConvexShape heart;
-            
-
-            heart.setPointCount(100);
+            std::vector<sf::Vector2f> points(100);
 
             for (int i = 0; i < 100; ++i){
 
@@ -47,14 +44,40 @@ class BreakHeartShape{
 
                 float y0 = 13 * std::cos(t) - 5 * std::cos(2 * t) - 2 * std::cos(3 * t) - std::cos(4 * t);
 
-                heart.setPoint(i, sf::Vector2f(x + x0 * scale, y - y0 * scale));
+                points[i] = sf::Vector2f(x + x0 * scale, y - y0 * scale);
             }
 
-            heart.setFillColor(sf::Color::Red);
+            sf::ConvexShape right;
+            right.setPointCount(51);
+            for (int j = 0; j <= 50; ++j){
+                right.setPoint(j, points[j]);
+            }
+            right.setFillColor(sf::Color::Red);
 
-            
+            sf::ConvexShape left;
+            left.setPointCount(51);
+            left.setPoint(0, points[50]);
+            for (int j = 1; j < 50; ++j){
+                left.setPoint(j, points[50 + j]);
+            }
+            left.setPoint(50, points[0]);
+            left.setFillColor(sf::Color::Red);
 
-            return heart;
+
+            float offset = 0.f;  
+
+            for (size_t i = 0; i < right.getPointCount(); ++i){
+                auto p = right.getPoint(i);
+                p.x += offset;
+                right.setPoint(i, p);
+            }
+            for (size_t i = 0; i < left.getPointCount(); ++i){
+                auto p = left.getPoint(i);
+                p.x -= offset;
+                left.setPoint(i, p);
+            }
+
+            return {left, right};
         }
 
 };
--- heart.cpp
+++ heart.cpp
@@ -12,9 +12,67 @@ int main(){
 
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Heart", sf::Style::Titlebar | sf::Style::Close);
 
-    //auto heart = HeartShape::create(400.f, 250.f, 10.f);
+    auto heart = HeartShape::create(400.f, 250.f, 10.f);
 
-    auto BreakHeart = BreakHeartShape::create(400.f, 250.f, 10.f);
+    //auto BreakHeart = BreakHeartShape::create(400.f, 250.f, 10.f);
+
+
+    // start broken heart
+    /*
+    float cx = 400.f;
+    float cy = 250.f;
+    float scale = 10.94f;
+    float pi = 3.141592f;
+
+    float t_top = 0.f;
+    float x0_top = 16 * std::pow(std::sin(t_top), 3);
+    float y0_top = 13 * std::cos(t_top) - 5 * std::cos(2 * t_top) - 2 * std::cos(3 * t_top) - std::cos(4 * t_top);
+    sf::Vector2f top(cx + x0_top * scale, cy - y0_top * scale);
+
+    float t_bottom = pi;
+    float x0_bottom = 16 * std::pow(std::sin(t_bottom), 3);
+    float y0_bottom = 15.74f * std::cos(t_bottom) - 5 * std::cos(2 * t_bottom) - 2 * std::cos(3 * t_bottom) - std::cos(4 * t_bottom);
+    sf::Vector2f bottom(cx + x0_bottom * scale, cy - y0_bottom * scale);
+
+    float num_segments = 8.8f;  
+    int num_points = num_segments + 1;
+    std::vector<sf::Vector2f> crack_points;
+    float zig_width = 8.f;  
+
+    for (int i = 0; i < num_points; ++i){
+        float fraction = static_cast<float>(i) / (num_points - 1);
+        float curr_y = top.y + fraction * (bottom.y - top.y);
+        float curr_dx = 0.f;
+        if (i != 0 && i != num_points - 1){
+            curr_dx = zig_width * ((i % 2 == 0) ? 1.f : -1.f);
+        }
+        crack_points.push_back({top.x + curr_dx, curr_y});
+    }
+
+
+    std::vector<sf::RectangleShape> crack_segments;
+    float thickness = 3.6f; 
+
+    for (size_t i = 0; i < crack_points.size() - 1; ++i){
+        sf::Vector2f p1 = crack_points[i];
+        sf::Vector2f p2 = crack_points[i + 1];
+        sf::Vector2f dir = p2 - p1;
+        float length = std::hypot(dir.x, dir.y);
+        if (length == 0) continue;
+        sf::RectangleShape seg(sf::Vector2f(length, thickness));
+        seg.setOrigin(sf::Vector2f(0.f, thickness / 2.f));
+        seg.setPosition(p1);
+        seg.setFillColor(sf::Color::Black);
+        float angle = std::atan2(dir.y, dir.x) * 180.f / pi;
+        seg.setRotation(sf::degrees(angle));
+        crack_segments.push_back(seg);
+    }
+    */
+    // end broken heart
+
+
+
+
 
     sf::Font font;
 
@@ -34,9 +92,9 @@ int main(){
 
     if (!music.openFromFile("assets/sound/sad_rap.wav")){
         MessageBoxW(NULL, L"Error: Unable to load 'sad_rap.wav'. The program will continue without music.", L"Audio loading error", MB_OK | MB_ICONWARNING);   
-    }else{
-        music.play();
     }
+
+    music.play();
 
     while (window.isOpen()){
 
@@ -58,7 +116,19 @@ int main(){
 
         window.clear(sf::Color::Black);
 
-        window.draw(BreakHeart);
+        // start broken heart
+        /*
+        for (const auto& part : BreakHeart) {
+            window.draw(part);
+        }
+
+        for (const auto& seg : crack_segments) {
+            window.draw(seg);
+        }
+        */
+        // end broken heart
+
+        window.draw(heart);
 
         window.draw(text);
 
